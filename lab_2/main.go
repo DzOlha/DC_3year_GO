a8cecc4e222c545b1c85d4458bca3d4dbe9f6bbe
DzOlha <monsarda11@gmail.com>
2022-09-17 19:17:21 +0300

    Completed the THIRD task of the second lab work!

--- lab_2/main.go
+++ lab_2/main.go
@@ -6,28 +6,29 @@ import (
 	"time"
 )
 
-const SIZE = 10
+const SIZE = 16
 
 type Player struct {
 	name      string
 	energy    int
-	monastery bool
+	monastery string
 }
 
 func fillPlayersArray() [SIZE]Player {
-	//false - first monastery
-	//true - second monastery
-	var monTmp bool = false
+	//M1 - first monastery
+	//M2 - second monastery
+	var monTmp string = "M1"
 
 	arr := [SIZE]Player{}
 
 	//set seed as timeNow to generate every time new values
 	rand.Seed(time.Now().UnixMicro())
+	fmt.Println("-----------------Initial Players------------------")
 	for i := 0; i < SIZE; i++ {
 		//the first part of the players is from the FIRST monastery
 		//the second part of the players is from the SECOND monastery
 		if i >= SIZE/2 {
-			monTmp = true
+			monTmp = "M2"
 		}
 		arr[i].name = fmt.Sprint("player_", i)
 		arr[i].energy = rand.Intn(100) + 1
@@ -35,10 +36,44 @@ func fillPlayersArray() [SIZE]Player {
 
 		fmt.Println(arr[i])
 	}
+	fmt.Println("-----------------Initial Players------------------")
 	return arr
 }
+
+func oneCompetition(one Player, two Player, c chan Player) {
+	if one.energy > two.energy {
+		one.energy += two.energy
+		two.energy = 0
+		c <- one
+	} else {
+		two.energy += one.energy
+		one.energy = 0
+		c <- two
+	}
+}
+
 func main() {
-	const size = 10
-	fillPlayersArray()
+	players := fillPlayersArray()
+	var halfSize int = SIZE / 2
+	var size int = SIZE
+	c := make(chan Player, halfSize)
+
+	for {
+		for i := 0; i < halfSize; i++ {
+			go oneCompetition(players[i], players[size-i-1], c)
+		}
+		for i := 0; i < halfSize; i++ {
+			players[i] = <-c
+			fmt.Println(players[i])
+			if i == halfSize-1 {
+				fmt.Println(fmt.Sprint("---------------------Above are Winners: Round 1/", size/2, "-------------------------"))
+			}
+		}
+		size = halfSize
+		if size == 1 {
+			break
+		}
+		halfSize = halfSize / 2
+	}
 
 }
